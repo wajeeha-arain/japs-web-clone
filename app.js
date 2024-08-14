a1e0ba5169cc7e5c19653b57852a50225c9960dc
Wajeeha M.Ashraf <arainwajeeha715@gmail.com>
2024-08-14 10:01:04 +0500

    add-files

---
+++ app.js
@@ -0,0 +1,55 @@
+const Signup =async(e)=>{ 
+    e.preventDefault()
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value
+    console.log(email, password);
+
+    try {
+        const result = await firebase.auth().createUserWithEmailAndPassword(email, password);
+        console.log(result);
+   
+        alert(` wellcom ${result.user.email}`)
+        
+    } catch (err) {
+        console.log(err);
+        alert(err.message)
+       
+        
+    }
+}
+
+
+const login = async (e) => {
+    e.preventDefault()
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value
+    // console.log(email, password);
+
+    try {
+        const result = await firebase.auth().signInWithEmailAndPassword( email, password)
+        
+        console.log(result);
+        alert(` user is successfullu login ${result.user.email}`)
+    } catch (err) {
+        alert(err.message)
+    }
+}
+const logout = ()=>{
+   
+    firebase.auth().signOut()
+
+      
+
+
+
+firebase.auth().onAuthStateChanged( (user) => {
+    if (user) {
+
+    } else {
+    console.log(`user successfully log out`);
+    alert(`user successfully log out`)
+   
+
+    }
+  })
+};
